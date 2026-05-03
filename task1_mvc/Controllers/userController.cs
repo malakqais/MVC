cf093777df338631ecd87b345c108fd519b64715
malakqais <fzgwmk6s97@privaterelay.appleid.com>
2026-05-03 15:19:57 +0300

    Add files via upload

---
+++ task1_mvc/Controllers/userController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task1_mvc.Controllers
+{
+    public class userController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
---
+++ task1_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace task1_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
